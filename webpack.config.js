00a7b65f3afbd4d24a8c85cb99a46450aa41209d
march-dave <ysangy@gmail.com>
2017-05-04 21:24:57 -0400

    webpack config error

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
+var webpack = require('webpack');
+
 var config = {
-   entry: './main.js',
-	
-   output: {
-      path:'/',
-      filename: 'index.js',
-   },
-	
-   devServer: {
-      inline: true,
-      port: 8080
-   },
-	
-   module: {
-      loaders: [
-         {
+    entry: './main.js',
+
+    output: {
+        path: '/',
+        filename: 'index.js',
+    },
+
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+
+    module: {
+        loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
             loader: 'babel',
-				
+
             query: {
-               presets: ['es2015', 'react']
+                presets: ['es2015', 'react']
             }
-         }
-      ]
-   }
+        }]
+    }
 }
 
-module.exports = config;
+// module.exports = config;
